6c22b17f0bf93a09d471d75a4ab63eff9543797a
IreneTaPa <italavero@hotmail.com>
2023-09-14 11:17:23 +0200

    fix css of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 import '../styles/Footer.scss';
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <p className="contact">contacto: italavero@hotmail.com</p>
+      <small className="copy">Irene Talavero ©</small>
+    </div>
+  );
 };
 
 export default Footer;
