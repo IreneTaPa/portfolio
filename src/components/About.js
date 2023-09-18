03de887d7423671d88b1144bc6f32719f59980d7
IreneTaPa <italavero@hotmail.com>
2023-09-18 13:27:05 +0200

    about and tech components

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,27 @@
 import '../styles/About.scss';
 import '../styles/Variables.scss';
+import img from '../images/photo.jpeg';
+import TechnologiesDetail from './TechnologiesDetail';
 const About = () => {
   return (
     <div className="body_about">
       <h1 className="about_title">ABOUT</h1>
+      <div className="div_letter">
+        <img src={img} alt="Author" className="author_img" />
+        <div className="letter">
+          Proveniente del sector farmacéutico, he tenido la oportunidad de
+          adquirir sólidos conocimientos a través de mi bootcamp en la escuela
+          Adalab, donde me he especializado en tecnologías web como HTML, CSS,
+          JavaScript, REACT... He trabajado en proyectos prácticos para crear
+          sitios web interactivos y amigables. Además, he colaborado en
+          diferentes equipos, lo que ha enriquecido mi capacidad para
+          comunicarme eficazmente y trabajar en entornos colaborativos. Un
+          aspecto que me enorgullece es haber sido parte de una iniciativa que
+          fomenta la diversidad y la inclusión en el campo de la tecnología.
+        </div>
+      </div>
+      <h1>TECHNOLOGIES</h1>
+      <ul className="technologies_list">{<TechnologiesDetail />}</ul>
     </div>
   );
 };
---
+++ src/components/TechnologiesDetail.js
@@ -0,0 +1,43 @@
+import '../styles/TechnologiesDetail.scss';
+import htmlLogo from '../images/html-logo.png';
+import cssLogo from '../images/css-logo.png';
+import sassLogo from '../images/saas-logo.png';
+import jsLogo from '../images/js-logo.png';
+import reactLogo from '../images/react-logo.png';
+import gulpLogo from '../images/gulp-logo.svg';
+import jestLogo from '../images/jest-logo.png';
+import nodeLogo from '../images/node-logo.jpg';
+import sqlLogo from '../images/mysql-logo.png';
+import mongoLogo from '../images/mongod-logo.png';
+import gitLogo from '../images/git-logo.png';
+import githubLogo from '../images/github-logo.png';
+
+const TechnologiesDetail = () => {
+  const imagesData = [
+    { id: 1, src: htmlLogo },
+    { id: 2, src: cssLogo },
+    { id: 3, src: sassLogo },
+    { id: 4, src: jsLogo },
+    { id: 5, src: reactLogo },
+    { id: 6, src: gulpLogo },
+    { id: 7, src: jestLogo },
+    { id: 8, src: nodeLogo },
+    { id: 9, src: sqlLogo },
+    { id: 10, src: mongoLogo },
+    { id: 11, src: gitLogo },
+    { id: 12, src: githubLogo },
+  ];
+  console.log(imagesData);
+
+  return (
+    <>
+      {imagesData.map((image) => (
+        <li key={image.id} className="technologies_elements">
+          <img src={image.src} alt="logo" className="technologies_images" />
+        </li>
+      ))}
+    </>
+  );
+};
+
+export default TechnologiesDetail;
